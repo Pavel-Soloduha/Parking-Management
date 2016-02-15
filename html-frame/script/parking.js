07be41393441acd7c6d693846dc8daba703df7f6
Pavel-Soloduha <shadowofclown@gmail.com>
2016-02-15 15:34:24 +0300

    finalize

---
+++ html-frame/script/parking.js
@@ -0,0 +1,38 @@
+function createParkingTable() {
+    $('#parkingtable').append('<table></table>');
+    var table = $('#parkingtable').children();
+    table.append("<tr>"
+    + "<td><a href=Parking name</td>"
+    + "<td>Amount free places</td>"
+    + "<td>Amount busy places</td>"
+    + "<td>Amount floors</td>"
+    + "<td>Parking address</td>"
+    + "<td>Description</td>"
+    + "<td style=\"display: none;\">parkingId</td>"
+    + "</tr>");
+    fillParkingTable();
+};
+function fillParkingTable() {
+    $.get("http://localhost:8080/parkingManagement/parking/", function(data, status){
+        var info = data.parkings;
+        var trHTML = '';
+        $.each(info, function (i, item) {
+            trHTML += '<tr><td>' + item.parkingName + '</td>'
+            + '<td>' + (item.amountPlace - item.amountBusy) + '</td>'
+            + '<td>' + item.amountBusy + '</td>'
+            + '<td>' + item.amountFloor + '</td>'
+            + '<td>' + item.address + '</td>'
+            + '<td>' + item.description + '</td>'
+            + '<td style="display: none;">' + item.parkingId + '</td>'
+            + '</tr>';
+        });
+        var table = $('#parkingtable').children();
+        table.append(trHTML);
+    });
+};
+
+$(document).ready(function (){
+   createParkingTable();
+   var colToHide = $tbl.find("." + $(this).attr("name"));
+   $(colToHide).toggle();
+});
